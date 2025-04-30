9ab36c9c98288907bf0868eb8fee7034c70933f6
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-04-30 13:08:50 +0200

    solved me prep'

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -3,12 +3,44 @@
  */
 
 const http = require('http');
+const fs = require('fs');
 
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+	if (req.url === '/') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		fs.readFile('./index.html', (err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			res.send(data);
+		})
+	}
+	if (req.url === '/index.js') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/javascript');
+		fs.readFile('./index.js', (err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			res.send(data);
+		})
+	}
+	if (req.url === '/styles.css') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/css');
+		fs.readFile('./styles.css', (err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			res.send(data);
+		})
+	}
+	res.end();
 });
 
 server.listen(3000); // The server starts to listen on port 3000
