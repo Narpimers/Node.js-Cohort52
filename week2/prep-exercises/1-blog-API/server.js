43b23f3a8601b8858c4f2d5933b678d74f17b729
Ilias Khugaev <big.ilyasus95@gmail.com>
2025-05-03 14:04:41 +0200

    solved prep

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,58 @@
-const express = require('express')
+const { error } = require('console');
+const express = require('express');
+const fs = require("fs");
 const app = express();
- 
 
-// YOUR CODE GOES IN HERE
+
+app.use(express.json());
+
+app.post('/blogs', (req, res) => {
+  const {title, content} = req.body;
+  fs.writeFileSync(title, content);
+  res.statusCode = 200;
+  res.end('ok');
+});
+
+app.put('/posts/:title', (req, res) => {
+  const {title, content} = req.body;
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.statusCode = 200;
+    res.end('ok');
+
+  } else {
+    res.statusCode = 404;
+    res.send('This post does not exist!', error);
+  }
+});
+
+app.delete('/blogs/:title', (req, res) => {
+  const {title} = req.body;
+  if (fs.existsSync(title)) {
+    fs.unlinkSync(title);
+    res.statusCode = 200;
+    res.end('ok');
+  } else {
+    res.statusCode = 404;
+    res.send('This post was deleted!', error);
+  }
+});
+
+
+app.get('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  if (fs.existsSync(title)){
+    const post = fs.readFileSync(title);
+    res.send(post);
+  } else {
+    res.statusCode = 404;
+    res.send(error);
+  }
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
  
 app.listen(3000)
